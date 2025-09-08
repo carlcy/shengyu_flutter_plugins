08780c62fdc0668e94201a2f5b00077cef399b9b
carlcy <carlcy2023@163.com>
2025-09-08 14:41:33 +0800

    fix: 更换extension目录

--- lib/images/image_utils.dart
+++ lib/images/image_utils.dart
@@ -2,7 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shengyu_flutter_plugins/enum/image_type.dart';
-import 'package:shengyu_flutter_plugins/util/extension/string_extension.dart';
+import 'package:shengyu_flutter_plugins/extension/string_extension.dart';
 
 class ImageUtils {
   static ImageProvider getAssetImage(String name, {ImageFormat format = ImageFormat.png, String? packageName}) {
